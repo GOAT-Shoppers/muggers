d0b911f4a34a7b882c16bd81590f611c7b2986fc
Gabby Josebachvili <gabbyjose@gmail.com>
2018-02-28 16:17:26 -0500

    updated address model

--- server/db/models/address.js
+++ server/db/models/address.js
@@ -4,20 +4,36 @@ const db = require('../db');
 const Address = db.define('address', {
   street: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isEmpty: false
+    }
   },
   state: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isEmpty: false
+    }
   },
   city: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isEmpty: false
+    }
   },
   zip: {
     type: Sequelize.INTEGER,
     allowNull: false,
+    validate: {
+      isEmpty: false
+    }
   }
 })
+
+Address.prototype.fullAddress = function() {
+  return `${this.street} ${this.state} ${this.city} ${this.zip}`
+}
 
 module.exports = Address;
