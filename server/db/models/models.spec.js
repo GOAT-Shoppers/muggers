aebf692c5533c342d05a1f7d02aab080ac27c2fe
Neha <cneha88@gmail.com>
2018-03-01 11:21:42 -0500

    modelTesting

---
+++ server/db/models/models.spec.js
@@ -0,0 +1,101 @@
+
+const { expect } = require('chai')
+const db = require('../index')
+const Product = require('./product')
+const Review = require('./review')
+const LineItem = require('./lineItem')
+const Order = require('./order')
+const Category = require('./category')
+
+
+describe('Order', () => {
+  describe('Attributes', () => {
+    const { attributes } = Order;
+    it('Has attributes as expected', () => {
+      expect(attributes.email).to.be.a('object');
+      expect(attributes.status).to.be.a('object');
+    });
+
+    describe('email attribute', () => {
+      const { attributes: { email } } = Order;
+      it('Validates email', () => {
+        expect(email.validate.isEmail).to.be.eql(true)
+      });
+    });
+    describe('Status attribute', () => {
+      const { attributes: { status} } = Order;
+      it('Has a default value which is shopping', () => {
+        expect(status.defaultValue).to.be.eql('shopping');
+      });
+    });
+  })
+})
+
+describe('Review', () => {
+  describe('Attributes', () => {
+    const { attributes } = Review;
+    it('Has attributes as expected', () => {
+      expect(attributes.text).to.be.a('object');
+      expect(attributes.rating).to.be.a('object');
+    });
+    describe('rating attribute', () => {
+      const { attributes: { rating } } = Order;
+      it('Validates rating', () => {
+        //expect(rating.validate.min).to.equal(0);
+      });
+    });
+ })
+})
+
+describe('Category', () => {
+  describe('Attributes', () => {
+    const { attributes } = Category;
+    it('Has attributes as expected', () => {
+      expect(attributes.name).to.be.a('object');
+    });
+  })
+})
+
+describe('LineItem', () => {
+  describe('Attributes', () => {
+    const { attributes } = LineItem;
+    it('Has attributes as expected', () => {
+      expect(attributes.quantity).to.be.a('object');
+      expect(attributes.price).to.be.a('object');
+    });
+  })
+})
+
+describe('Product', () => {
+  describe('Attributes', () => {
+    const { attributes } = Product;
+    it('Has attributes as expected', () => {
+      expect(attributes.name).to.be.a('object');
+      expect(attributes.description).to.be.a('object');
+      expect(attributes.price).to.be.a('object');
+      expect(attributes.stock).to.be.a('object');
+      expect(attributes.photo).to.be.a('object');
+    });
+  })
+})
+
+//   before('Await database sync', () => db.didSync)
+//   afterEach('Clear the tables', () => db.truncate({ cascade: true }))
+
+//   describe('Order Models', () => {
+//     let order;
+
+//     before('create order', function () { order = Order.create() })
+//     it('require email', () => {
+//       return order
+//         .then(() => { throw new Error('Promise should have rejected'); })
+//         .catch(err => {
+//           expect(err).to.be.an('object');
+//           expect(err.errors).to.contain.a.thing.with.properties({
+//             path: 'email',
+//             type: 'notNull Violation'
+//           });
+//       });
+//    })
+//   })
+// })
