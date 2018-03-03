ceaabf78e4f3519de2a276578b91b112a256cd3f
Veronica Smith <veronica.smith2009@yahoo.com>
2018-03-03 01:41:50 -0500

    homeComp-#78

---
+++ client/components/Home.jsx
@@ -0,0 +1,29 @@
+import react, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchProducts } from '../store';
+
+export class Home extends Component {
+    componentDidMount() {
+        this.props.loadProducts();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Welcome USER</h1>
+                <div>IMAGE</div>
+                <div>PRODUCTS</div>
+            </div>
+        )
+    }
+}
+
+const mapState = state => ({
+    products: state.products
+});
+
+const mapDispatch = dispatch => ({
+    loadProducts: () => dispatch(fetchProducts())
+});
+
+export default connect(mapState, mapDispatch)(Home);
