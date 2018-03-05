abf78b56493abe9d15c463b8804fd327e10517f6
vsmith214 <34035004+vsmith214@users.noreply.github.com>
2018-03-05 11:47:06 -0500

    Single prod comp #73 (#100)
    * productreducer-#88
    
    * homeComp-#78
    
    * categoryReducer-#89
    
    * homeComp-#78
    
    * addressReducer-#92
    
    * newAddressComp-#86
    
    * allProductsComp-#72
    
    * singleProdComp-#73
    
    * singleProdComp-#73

---
+++ client/components/AllProducts.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+
+export const AllProducts = props => {
+
+        const { products } = props;
+        return (
+        <div>
+            <div>
+                <h1>All Products</h1>
+            </div>
+            <div>
+                {
+                    products.map(product => <Link to={`/products/${product.id}`} key={product.id}><h3>{product.name}</h3></Link>)
+                }
+            </div>
+            {/* Only render below if Admin */}
+            {/* <div> 
+                <Link to={'/addProduct'}>Add a product!</Link>
+            </div> */}
+        </div>
+        )
+}
+
+const mapState = state => ({
+    products: state.products
+});
+
+export default connect(mapState, null)(AllProducts);
+
--- client/routes.js
+++ client/routes.js
@@ -1,9 +1,13 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch} from 'react-router-dom'
-import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
-import {me} from './store'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Login,
+        Signup,
+        UserHome,
+        AllProducts,
+        SingleProduct } from './components';
+import {me} from './store';
 
 /**
  * COMPONENT
@@ -21,6 +25,8 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route exact path="/products" component={AllProducts} />
+        <Route path="/products/:id" component={SingleProduct} />
         {
           isLoggedIn &&
             <Switch>
