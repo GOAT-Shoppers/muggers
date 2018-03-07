aa299441c43b67ebf06d50b2a1c843bea53b9a8b
Veronica Smith <veronica.smith2009@yahoo.com>
2018-03-06 22:05:17 -0500

    Add to cart working for logged in user

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,42 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import { fetchActiveOrder } from '../store';
 
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {fullName, email} = props;
+class UserHome extends Component {
 
-  let name;
-    if (fullName === 'null null') {
-      if (!email) name = 'Guest';
-      else name = email;
-    }
-    else { name = fullName; }
-
-  return (
-    <div>
-      <h3>Welcome, {name}</h3>
-      <div className="homepage genericBackground">
-        <p>Feel free to browse our mug selection up top.</p>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Teacup_clipart.svg/672px-Teacup_clipart.svg.png" />
+  componentDidMount() {
+    this.props.loadOrder(this.props.user.id);
+  }
+  render() {
+    const {fullName, email} = this.props;
+  
+    let name;
+      if (fullName === 'null null') {
+        if (!email) name = 'Guest';
+        else name = email;
+      }
+      else { name = fullName; }
+  
+    return (
+      <div>
+        <h3>Welcome, {name}</h3>
+        <div className="homepage genericBackground">
+          <p>Feel free to browse our mug selection up top.</p>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Teacup_clipart.svg/672px-Teacup_clipart.svg.png" />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-/**
- * CONTAINER
- */
+const mapDispatch = dispatch => {
+  return {
+    loadOrder: id => dispatch(fetchActiveOrder(id))
+  }
+}
 export const mapState = state => {
   return {
     email: state.user.email,
-    fullName: state.user.fullName
+    fullName: state.user.fullName,
+    user: state.user
   };
 }
 
-export default connect(mapState)(UserHome);
+export default connect(mapState, mapDispatch)(UserHome);
 
 /**
  * PROP TYPES
