279650ce0b985a21543ee2a631b8dba8551b88b8
Mimi Zhao <mimi.zhao@berkeley.edu>
2018-03-06 23:27:59 -0500

    Fixed some key issues

--- client/components/Admin/User.js
+++ client/components/Admin/User.js
@@ -1,26 +1,26 @@
 import React from "react";
 
+const makeRow = (user) =>
+  (
+    <div key={user.id}>
+      <hr />
+      <div className="userContainer">
+
+        <p>  UserId: </p>
+        <p> {user.id}  </p>
+        <p>  Name: </p>
+        <p> {user.fullName}</p>
+        <p>  Email: </p>
+        <p> {user.email}</p>
+        <p> {user.isAdmin ? 'Admin' : 'User'}</p>
+      </div>
+    </div>
+  )
 
 export default function User (props){
-console.log(props.user)
   return (
     <div>
       {props.users && props.users.map(user => makeRow(user))}
      </div>
   );
 }
-
-const makeRow = (user) =>
-  <div>
-    <hr />
-    <div key= {user.id} className="userContainer">
-
-      <p>  UserId: </p>
-      <p> {user.id}  </p>
-      <p>  Name: </p>
-      <p> {user.fullName}</p>
-      <p>  Email: </p>
-      <p> {user.email}</p>
-      <p> {user.isAdmin ? 'Admin' : 'User'}</p>
-      </div>
-    </div>
